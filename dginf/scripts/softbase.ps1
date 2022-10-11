4244bd4376f2c56c507b8a7abdf7d7c80bb3c498
joker-87 <miguel.garay87@gmail.com>
2022-10-11 11:54:43 -0300

    add files

---
+++ dginf/scripts/softbase.ps1
@@ -0,0 +1,7 @@
+winget install Mozilla.Firefox -h
+winget install Google.Chrome -h
+winget install Adobe.Acrobat.Reader.64-bit -h
+winget install 7zip.7zip -h
+winget install RARLab.WinRAR -h
+winget install RustDesk.RustDesk -h
+winget install -e --id Microsoft.dotNetFramework -h
---
+++ jokerpc/scripts/softbase.ps1
@@ -0,0 +1,9 @@
+winget install Mozilla.Firefox -h
+winget install Google.Chrome -h
+winget install Adobe.Acrobat.Reader.64-bit -h
+winget install 7zip.7zip -h
+winget install RARLab.WinRAR -h
+winget install RustDesk.RustDesk -h
+winget install VideoLAN.VLC -h
+winget install AIMP.AIMP -h
+winget install -e --id Microsoft.dotNetFramework -h
