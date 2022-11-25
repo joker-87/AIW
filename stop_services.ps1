0ebb70b520b987faa91413701f7b6993a540d589
joker-87 <miguel.garay87@gmail.com>
2022-11-25 08:51:56 -0300

    cleanup

--- stop_services.ps1
+++
@@ -1,6 +0,0 @@
-[string[]]$services = @('SysMain', 'WSearch', 'DiagTrack', 'dmwappushservice', 'MapsBroker', 'RemoteRegistry', 'Fax', 'BDESVC')
-Stop-Service -Name $services -NoWait -Force
-while ((Get-Service -Name $services | Where-Object { $_.status -ne 'Stopped'}).Count -ne 0)
-{
-    Start-Sleep -Seconds 5
-}
